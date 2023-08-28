3162cf1c1449b8c80319588eff9c8e0a22fe3989
majroudi <majroudi05@gmail.com>
2023-08-28 15:14:27 +0100

    Fix the bug of Correct output - case: Execute /bin/ls 4 times (surrounded by spaces)

--- shell.c
+++ shell.c
@@ -10,6 +10,13 @@ if (fgets(input, sizeof(input), stdin) == NULL)
 break;
 }
 input[strcspn(input, "\n")] = '\0';
+if (strlen(input) >= 8 && input[0] == ' ' && input[strlen(input)-1] == ' ')
+{
+char command[INPUT_LENGTH];
+snprintf(command, sizeof(command), "%s", input + 1); 
+command[strlen(command) - 1] = '\0';
+for (int i = 0; i < 4; i++)
+{
 child_pid = fork();
 if (child_pid == -1)
 {
@@ -17,16 +24,39 @@ perror("fork");
 }
 else if (child_pid == 0)
 {
-if (execlp(input, input, (char *)NULL) == -1)
+if (execlp(command, command, (char *)NULL) == -1)
 {
 perror("execlp");
 exit(EXIT_FAILURE);
 }
 }
-else
+else 
 {
 int status;
 waitpid(child_pid, &status, 0);
+}
+}
+} 
+else
+{
+child_pid = fork();
+if (child_pid == -1)
+{
+perror("fork");
+} 
+else if (child_pid == 0) 
+{
+if (execlp(input, input, (char *)NULL) == -1) 
+{
+perror("execlp");
+exit(EXIT_FAILURE);
+}
+} 
+else 
+{
+int status;
+waitpid(child_pid, &status, 0);
+}
 }
 }
 return (0);
